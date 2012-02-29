ef6ca7b7ce2944cf9572edeeef1b354f081cb677
mohayonao <mohayonao@gmail.com>
2012-02-29 20:14:12 +0900

    req.host -> req.headers.host

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var timerMap = {};
 
 app.get("/", function(req, res) {
     var result = false;
-    if (req.host === "mohayonao.herokuapp.com") {
+    if (req.headers.host === "mohayonao.herokuapp.com") {
         if (/(iphone)/i.test(req.headers["user-agent"])) {
             result = sendDorilaSound(req, res);
         }
